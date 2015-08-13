c229f74df28d94e929d8629eddaecaca27e75e38
Fadi-Hakim <Fadihakim@msn.com>
2015-08-13 18:07:19 +0100

    Saintizing User Inputs

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require 'sinatra/flash'
 
 
 class MyApp < Sinatra::Base
 
   enable :sessions
+  register Sinatra::Flash
 
   set :session_secret, 'super secret'
 
@@ -37,16 +39,22 @@ class MyApp < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                       password: params[:password],
                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb :'users/new'
+    end
   end
 
   helpers do
